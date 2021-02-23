931b5c2ee191b3500b0c3b29cdef53a291ce183b
Alex <tinarskiy@bk.ru>
2021-02-23 16:36:59 +0300

    1.0.2

--- source/fonts/readme.txt
+++
@@ -1,1 +0,0 @@
-http://www.font2web.com
--- source/script.js
+++ source/script.js
@@ -1,22 +1,3 @@
 import "./style.scss";
 
-// import $ from "jquery";
-//
-// import "./components/slick-carousel/slick"
-//
-// import "@fancyapps/fancybox"
-//
-// import "./components/owl-carousel/owl.carousel"
-
-
-// Образец
-
-// import Vue from 'vue'
-// import App from './vue/App.vue'
-
-// new Vue({
-//   el: '#app',
-//   render: h => h(App)
-// })
-
 import "./components/calendar/calendar"
